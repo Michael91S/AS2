2fad46d88104c0499a37d566bacc65251aa95c42
Michael Spain <m.spain@setonhill.edu>
2023-03-31 20:13:30 -0400

    done

--- AS2/ViewController.swift
+++ AS2/ViewController.swift
@@ -9,6 +9,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Name: UITextField!
+    
+    @IBOutlet weak var Food: UITextField!
+    
+    @IBOutlet weak var Drink: UITextField!
+    
+    
+    @IBOutlet weak var FriendName: UITextField!
+    
+    @IBOutlet weak var Place: UITextField!
+    
+    @IBOutlet weak var Label: UILabel!
+    
+    @IBAction func CreateMadLib(_ sender: UIButton) {
+        Label.text = Name.text! + " ate " + Food.text!+" and they drink " + Drink.text! + " with their friend " + FriendName.text! + " at " + Place.text!+" . "
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
